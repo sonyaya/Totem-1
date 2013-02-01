0b6b8dd464e0b8a5b81326c9cbcd62d9180f5fcf
vanuatu <gimmeoanel261>
2013-02-01 18:03:35 -0200

    Alterada a aperencia na listagem, agora existe um estilo parecido com o do momento da inserção e atualização;

--- system/backend/types/jsonArray/jsonArray.php
+++ system/backend/types/jsonArray/jsonArray.php
@@ -1,6 +1,6 @@
 <?php
     class jsonArray{
-                /**
+        /**
          * 
          * @param type $thisData
          * @param type $thisColumn
@@ -14,5 +14,16 @@
                 "val" => htmlentities($thisData),
                 "lis" => json_decode($thisData)
             );
+        }
+        
+        /**
+         * 
+         * @param type $thisData
+         * @param type $thisRow
+         * @param type $thisColumn
+         * @param type $allData
+         */
+        public function beforeList(&$thisData, $thisRow, $thisColumn, &$allData){
+            $thisData = "<div class='jsonArray'><ul class='list'><li>".implode("</li><li>", json_decode($thisData))."</li></ul></div>";
         }
     }
