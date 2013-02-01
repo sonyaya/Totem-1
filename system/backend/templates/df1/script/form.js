1788d9b43fe99e9859fd1a7985d43d95cb970822
vanuatu <gimmeoanel261>
2013-02-01 12:15:50 -0200

    Agora os formulário de atualização no layout df1 abrem em abas, caso o formulário de atualização seja diferente do atual abre em um popup;

---
+++ system/backend/templates/df1/script/form.js
@@ -0,0 +1,30 @@
+$(function(){
+    $("body").on("click", ".bt-save-form", function(e){
+        alert("bt save");
+        $form = $(this).closest("form");
+        $form.submit();
+        return false;
+    });
+});
+
+//
+function saveForm(form){
+    alert("submit do form");
+    $.post(
+        "?action=save-form&form=" + layout.uri("form"),
+        $(form).serialize(),
+        function(data){
+            if( data.error ){
+                mesageConcat = 'Os seguintes erros ocorreram: \r\n';
+                $.each(data.message, function(key, val){
+                    mesageConcat += "- "+val+"\r\n";
+                });
+                alert(mesageConcat);
+            }else{
+                alert("mensagem aqui");
+            }
+        },
+        "json"
+    );
+    return false;
+}
