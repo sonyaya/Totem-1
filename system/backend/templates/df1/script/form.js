5bcc76200f3f1622eb1b193317fbcd50484e9128
vanuatu <gimmeoanel261>
2013-02-05 16:03:24 -0200

    Mdado forma que o formulário é enviado.

--- system/backend/templates/df1/script/form.js
+++ system/backend/templates/df1/script/form.js
@@ -1,30 +1,32 @@
 $(function(){
-    $("body").on("click", ".bt-save-form", function(e){
+    //
+    $("body").on("click", ".bt-save-form", function(){
         $form = $(this).closest("form");
         $form.submit();
         return false;
     });
-});
 
-//
-function saveForm(form){
-    layout.ajax.showLoader();
-    $.post(
-        "?action=save-form&form=" + layout.uri("form"),
-        $(form).serialize(),
-        function(data){
-            if( data.error ){
-                mesageConcat = 'Os seguintes erros ocorreram: \r\n';
-                $.each(data.message, function(key, val){
-                    mesageConcat += "- "+val+"\r\n";
-                });
-                alert(mesageConcat);
-                layout.ajax.hideLoader();
-            }else{
-                layout.ajax.hideLoader();
-            }
-        },
-        "json"
-    );
-    return false;
-}
+    //
+    $("body").on("submit", "form.form-insert-or-update", function(){
+        layout.ajax.showLoader();
+        $.post(
+            "?action=save-form&form=" + layout.uri("form"),
+            $(this).serialize(),
+            function(data){
+                if( data.error ){
+                    mesageConcat = 'Os seguintes erros ocorreram: \r\n';
+                    $.each(data.message, function(key, val){
+                        mesageConcat += "- "+val+"\r\n";
+                    });
+                    alert(mesageConcat);
+                    layout.ajax.hideLoader();
+                }else{
+                    layout.ajax.hideLoader();
+                }
+            },
+            "json"
+        );
+
+        return false;
+    });
+});
