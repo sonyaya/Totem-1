6af144a60620d3f4f8fddd173bcd94d1e0125038
vanuatu <gimmeoanel261>
2013-02-07 16:46:18 -0200

    Criando dashboard.

--- system/backend/_system/class/DashboardComposer.php
+++ system/backend/_system/class/DashboardComposer.php
@@ -10,29 +10,33 @@
         
         private $dashboard = Array();
         private $dashboardData = Array();
+        private $dashboardConfig = Array();
+        
         
         public function viewDashboard($dashFilename, $templateHtml){
-            global $_M_CONFIG;
-            global $_M_THIS_CONFIG;
-            
-            // IMPORTA E RODA A CLASSE DE DASHBOARD
-            if(file_exists($filename = "dashboards/$dashFilename.php") ){
-                require_once $filename;
-                $this->dashboard = new \Dashboard();
-                $this->dashboardData = $this->dashboard->getData();
-            }else{
-                echo "Dashboard $filename não foi encontrado.";
-                exit;
-            }
-            
-            // Array To Layout
             global $_M_THIS_CONFIG;
             global $_M_MENU;
             global $_M_MENU_PARTS;
             global $_M_MENU_MODULE;
             global $_M_USER;
             
-            // monta o array
+            // IMPORTA E RODA A CLASSE DE DASHBOARD E CONFIGURAÇÕES
+            if(
+                file_exists($filenameDash = "dashboards/$dashFilename.php") &&
+                file_exists($filenameYaml = "dashboards/$dashFilename.yml")
+            ){
+                // calsse do dashboard
+                require_once $filenameDash;
+                $this->dashboard = new \Dashboard();
+                $this->dashboardData = $this->dashboard->getData();
+                
+                // arquivo de configuração
+                $this->dashboardConfig = Yaml::parse(file_get_contents($filenameYaml));
+            }else{
+                echo "Um dos arquivos de Dashboard não foram encontrado '$filenameDash' ou '$filenameYaml'.";
+                exit;
+            }
+            // Array To Layout
             $arrayBase = Array(  
                 "user" => $_M_USER ,
                 "main-menu" => $_M_MENU ,
@@ -41,17 +45,49 @@
                 "dashboard" => $dashFilename
             );
             
-            // GERA O HTML
-            echo new 
-                Frontend(
-                    $_M_THIS_CONFIG['template'] ."/". $templateHtml, 
-                    array_merge(
-                        $_M_THIS_CONFIG,
-                        $arrayBase,
-                        $this->dashboardData
-                    )
+            // MODULE FOLDER
+            $moduleFolder = preg_replace("/(.*)\/.*$/i", "$1", $dashFilename);
+            
+            // JS HEAD
+            $jsHead = Array();
+            foreach($this->dashboardConfig['interface']['javascript']['head'] as $file){
+                $jsHead[] = "dashboards/$moduleFolder/$file";
+            }
+            
+            // JS BODY
+            $jsBody = Array();
+            foreach($this->dashboardConfig['interface']['javascript']['body'] as $file){
+                $jsBody[] = (string)new Frontend("dashboards/$moduleFolder/$file", $arrayBase);
+            }
+            
+            // CSS HEAD
+            $styles = Array();
+            foreach($this->dashboardConfig['interface']['css'] as $file){
+                $styles[] = "dashboards/$moduleFolder/$file";
+            }
+            
+            // 
+            $arrayBase["css"] = $styles;
+            $arrayBase["javascript"]["head"] = $jsHead;
+            $arrayBase["javascript"]["body"] = $jsBody;
+            
+            // GERA O ARRAY PARA LAYOUT
+            $toLayout = array_merge(
+                    $_M_THIS_CONFIG,
+                    $arrayBase,
+                    $this->dashboardData
                 )
             ;
+            
+            // 
+            if( file_exists($fileHtml = "dashboards/$moduleFolder/{$this->dashboardConfig['interface']['html']}" ) ){
+                $toLayout["dashboardHtml"] = (string)new Frontend($fileHtml, $toLayout);
+            }else{
+                 $toLayout["dashboardHtml"] = "'$fileHtml' não foi encontrado.";
+            }
+            
+            // GERA O HTML
+            echo new Frontend($_M_THIS_CONFIG['template'] ."/". $templateHtml, $toLayout);
         }
         
     }
