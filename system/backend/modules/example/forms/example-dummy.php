2d978288655c1194e23b5f1f5ee2f87c3fd1266d
vanuatu <gimmeoanel261>
2013-03-01 08:52:11 -0300

    Exemplo de formulário dummy.

--- system/backend/modules/example/forms/example-dummy.php
+++ system/backend/modules/example/forms/example-dummy.php
@@ -1,15 +1,30 @@
 <?php
-
-    $html = "<pre>" . print_r($_POST, true) . "</pre>";
+    error_reporting(0);
 
     use vendor\mpdf\pdf;
 
+    $html = "<h1>Example</h1><pre>" . print_r($_POST, true) . "</pre>";
+    
+
+
+//    // RETORNO EM HTML
+//    return Array(
+//        "title" => "Retorno em HTML",
+//        "mime" => "text/html",
+//        "file" => base64_encode($html)
+//    );
+    
+    // RETORNO EM PDF
     $mpdf = new PDF(); 
     $mpdf->WriteHTML($html);
     $pdfFile = $mpdf->Output('', 'S');
+    return Array(
+        "title" => "Retorno em PDF",
+        "mime" => "application/pdf",
+        "file" =>  base64_encode($pdfFile)
+    );
     
-    die( '<object data="data:application/pdf;base64,'. base64_encode($pdfFile) .'" type="application/pdf" width="100%" height="100%"></object>' );
-    
+//    // CASO DE ERRO
 //    return Array(
 //        "error"     => true,
 //        "message"   => "Essa é uma mensagem de erro criada pelo programador do formulário dummy."
