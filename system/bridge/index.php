acac8d480d535d9ba47a85e469507ed04c184436
vanuatu <gimmeoanel261>
2013-03-01 18:05:46 -0300

    ...

--- system/bridge/index.php
+++ system/bridge/index.php
@@ -15,19 +15,5 @@
            )
        )
     );
-    
-    
-    # LAYOUT
-    if(  isset($_GET['_m_html']) &&  !empty($_GET['_m_html']) ){
-        if( file_exists($file = "{$sys['config']['html-folder']}/{$_GET['_m_html']}") ){
-            echo new Frontend($file, $sys);
-        }else{
-            trigger_error("O arquivo '$file' não foi encontrado.", E_USER_ERROR);
-        }
-    }else{
-        if( file_exists($file = "{$sys['config']['html-folder']}/{$sys['config']['html-start']}") ){
-            echo new Frontend($file, $sys);
-        }else{
-            trigger_error("O arquivo  padrão '$file' não foi encontrado.", E_USER_ERROR);
-        }
-    }
+        
+    # RUN
