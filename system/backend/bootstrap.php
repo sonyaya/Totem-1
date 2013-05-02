822b9a635a4e9adee658c0657e93565deaa35247
vanuatu <gimmeoanel261>
2013-05-02 16:08:26 -0300

    Separado log de erros fatais de logs de erros comuns.

--- system/backend/bootstrap.php
+++ system/backend/bootstrap.php
@@ -84,11 +84,69 @@
 
     date_default_timezone_set( $_M_CONFIG->system['time-zone'] );
 
-    # -- SAVE PHP ERROS IN A FILE ----------------------------------------------
+    # -- ERROR CONTROL ---------------------------------------------------------
     
     if( $_M_CONFIG->system['log-php-errors'] ){
+        # -- SAVE PHP FATAL ERROS IN A FILE ------------------------------------
+    
         error_reporting(E_ALL);
         ini_set ("display_errors" , "off");
         ini_set ("log_errors"     , "On");
-        ini_set ("error_log"      , "logs/".date('Y-m')."_-_php-errors.txt");
+        ini_set ("error_log"      , "logs/".date('Y-m')."_-_fatal-errors.txt");
+        
+        # -- SAVE ERROS IN A FILE ----------------------------------------------
+                
+        set_error_handler(
+            function($errno, $errstr, $errfile, $errline){
+
+                if( !file_exists($totemErrorFile = "logs/".date('Y-m')."_-_errors.md") ){
+                    $md  = "| DATE                | System  | Error Num. | Error Type                                      | Error Line | Description                                                                                                                   | file                                                                                                                          | \r\n";
+                    $md .= "|:-------------------:|:-------:|:----------:|:-----------------------------------------------:| ----------:|:----------------------------------------------------------------------------------------------------------------------------- |:----------------------------------------------------------------------------------------------------------------------------- | \r\n"; 
+                }else{
+                    $md = file_get_contents($totemErrorFile);
+                }
+
+                $md = trim($md);
+                $date = date("d/m/Y H:i:s");        
+                $errno   = str_pad( str_pad($errno  , 4, " ", STR_PAD_LEFT) , 10 , " ", STR_PAD_BOTH);
+                $errline = str_pad($errline, 10 , " ", STR_PAD_LEFT);
+                $errstr  = str_pad($errstr , 125, " ");
+                $errfile = str_pad($errfile, 125, " ");
+
+                switch ($errno) {
+                    case E_USER_ERROR:
+                        $md .= "\r\n| $date |  TOTEM  | $errno | <font color=#E13C26> E_USER_ERROR        </font> | $errline | $errstr | $errfile |";
+                        break;      
+
+                    case E_USER_WARNING:      
+                        $md .= "\r\n| $date |  TOTEM  | $errno | <font color=#F88B1C> E_USER_WARNING      </font> | $errline | $errstr | $errfile |";
+                        break;      
+
+                    case E_USER_NOTICE:      
+                        $md .= "\r\n| $date |  TOTEM  | $errno | <font color=#3171B2> E_USER_NOTICE       </font> | $errline | $errstr | $errfile |";
+                        break;      
+
+                    case E_WARNING:      
+                        $md .= "\r\n| $date |   PHP   | $errno | <font color=#F88B1C> E_WARNING           </font> | $errline | $errstr | $errfile |";
+                        break;      
+
+                    case E_NOTICE:      
+                        $md .= "\r\n| $date |   PHP   | $errno | <font color=#6A9D27> E_NOTICE            </font> | $errline | $errstr | $errfile |";
+                        break;
+
+                    case E_RECOVERABLE_ERROR:
+                        $md .= "\r\n| $date |   PHP   | $errno | <font color=#E13C26> E_RECOVERABLE_ERROR </font> | $errline | $errstr | $errfile |";
+                        break;
+
+
+                    default:
+                        $md .= "\r\n| $date |   ???   | $errno | <font color=#000000> UNKNOWN             </font> | $errline | $errstr | $errfile |";
+                        break;
+                }
+
+                file_put_contents($totemErrorFile, $md);
+
+                return true;
+            }
+        );
     }
