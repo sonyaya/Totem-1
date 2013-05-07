0b4ceb401d04d9b8291ee409684f8e35960fe818
vanuatu <gimmeoanel261>
2013-05-07 13:37:41 -0300

    Adicionado ID da sessão e melhorado a forma de apresentação de dados antigos

--- system/_commons/_system/class/Log.php
+++ system/_commons/_system/class/Log.php
@@ -3,7 +3,7 @@
     
     class Log{
 
-        public static function log($action, $msg, $oldData="..."){ 
+        public static function log($action, $msg, $oldData="…"){ 
             if( !file_exists($totemErrorFile = "logs/".date('Y-m')."_-_log.md") ){
                $md  = "| Date                | Session ID                            | User                             | Action                            | Message                                                                                                                                                                               | Backup Data \r\n";
                $md .= "|:-------------------:|:------------------------------------- | --------------------------------:|:---------------------------------:|:------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------\r\n"; 
@@ -20,9 +20,8 @@
             $msg = trim($msg);
             $msg = str_pad($msg, 184, " ", STR_PAD_RIGHT);
             
-            $oldData = (is_array($oldData)) ? json_encode($oldData) : (!empty($oldData))? $oldData : "…" ;
+            $oldData = (is_array($oldData)) ? json_encode($oldData) : $oldData;
             $oldData = trim($oldData);
-            $oldData = str_pad($oldData, 182, " ");
             
             $user = (!empty($_SESSION['user']['login']))? $_SESSION['user']['login'] : "…" ;
             $user = str_pad($user, 32, " ", STR_PAD_LEFT);
