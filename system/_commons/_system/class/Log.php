1ea2dea676fe690af33f4f34311460f26d322665
vanuatu <gimmeoanel261>
2013-05-07 09:41:40 -0300

    Adicionado ID da sessão atual

--- system/_commons/_system/class/Log.php
+++ system/_commons/_system/class/Log.php
@@ -5,8 +5,8 @@
 
         public static function log($action, $msg, $oldData="..."){ 
             if( !file_exists($totemErrorFile = "logs/".date('Y-m')."_-_log.md") ){
-               $md  = "| Date                | User                             | Action                      | Message                                                                                                                                                                               | Backup Data                                                                                                                                                                            |\r\n";
-               $md .= "|:-------------------:| --------------------------------:|:---------------------------:|:------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |\r\n"; 
+               $md  = "| Date                | Session ID                            | User                             | Action                            | Message                                                                                                                                                                               | Backup Data \r\n";
+               $md .= "|:-------------------:|:------------------------------------- | --------------------------------:|:---------------------------------:|:------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ------------\r\n"; 
             }else{
                $md = file_get_contents($totemErrorFile);
             }
@@ -18,7 +18,7 @@
             $action = str_pad( strtoupper($action) , 33, " ");
             
             $msg = trim($msg);
-            $msg = str_pad($msg, 184, " ");
+            $msg = str_pad($msg, 184, "-", STR_PAD_RIGHT);
             
             $oldData = (is_array($oldData)) ? json_encode($oldData) : $oldData;
             $oldData = trim($oldData);
@@ -27,7 +27,10 @@
             $user = (!empty($_SESSION['user']['login']))? $_SESSION['user']['login'] : "..." ;
             $user = str_pad($user, 32, " ", STR_PAD_LEFT);
             
-            $md .= "\r\n| $date | $user | $action | $msg | $oldData |";
+            $session = session_id();
+            $session = str_pad($session, 37, " ");
+        
+            $md .= "\r\n| $date | $session | $user | $action | $msg | $oldData";
             
             file_put_contents($totemErrorFile, $md);
         }
